63387b8714641375e36bd3906eefca46572944a7
Margubur Rahman <margubur@google.com>
2025-10-14 14:59:46 +0000

    Migrate gsutil usage to gcloud storage

--- courses/developingapps/v1.3/nodejs/firebase/start/prepare_environment.sh
+++ courses/developingapps/v1.3/nodejs/firebase/start/prepare_environment.sh
@@ -15,7 +15,8 @@ echo "Creating App Engine app"
 gcloud app create --region "us-central"
 
 echo "Making bucket: gs://$DEVSHELL_PROJECT_ID-media"
-gsutil mb gs://$DEVSHELL_PROJECT_ID-media
+# Note: The gcloud storage buckets create command is the direct equivalent of gsutil mb. The various aliases for gsutil mb are not supported.
+gcloud storage buckets create gs://$DEVSHELL_PROJECT_ID-media
 
 echo "Exporting GCLOUD_PROJECT and GCLOUD_BUCKET"
 export GCLOUD_PROJECT=$DEVSHELL_PROJECT_ID
